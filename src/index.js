2b03ecf35e0e6520280b96a440c75e831e67d5ce
c4seyla <c4seyla@gmail.com>
2020-03-04 14:27:32 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,6 +19,27 @@ import {
 	Affix
 } from 'antd';
 import 'antd/dist/antd.css';
+import { Link } from 'react-router-dom';
+
+import { userService } from '../_services';
+
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            user: {},
+            users: []
+        };
+    }
+
+    componentDidMount() {
+        this.setState({ 
+            user: JSON.parse(localStorage.getItem('user')),
+            users: { loading: true }
+        });
+        // userService.getAll().then(users => this.setState({ users }));
+    }
 
 
 function getNestedValue(obj, path) {
